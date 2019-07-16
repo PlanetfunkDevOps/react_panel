fc93cc146ff19c2dad230d50cd8fc027c691d474
Kev Artik <38816943+PlanetfunkDevOps@users.noreply.github.com>
2019-07-15 19:26:56 -0500

    Update store.js

--- src/store.js
+++ src/store.js
@@ -9,13 +9,7 @@ import settingsReducer from './redurcers/settingsReducer';
 
 /* Firebase config */
 const firebaseConfig = {
-  apiKey: 'AIzaSyDTZzXTxYStVdSw9AesLZyebZE_iqBQ9SM',
-  authDomain: 'reactclientpanel-2ce68.firebaseapp.com',
-  databaseURL: 'https://reactclientpanel-2ce68.firebaseio.com',
-  projectId: 'reactclientpanel-2ce68',
-  storageBucket: 'reactclientpanel-2ce68.appspot.com',
-  messagingSenderId: '147260590335',
-  appId: '1:147260590335:web:fe9b92891f11773d'
+  /+ Your firebase key goes here */
 };
 
 /* react-redux-firebase config */
